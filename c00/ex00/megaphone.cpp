b0ff352779555a5476ddf8057089d475e99261ee
Anton home <xd@xd.xd>
2022-05-12 20:02:46 +0200

    commit

--- c00/ex00/megaphone.cpp
+++ c00/ex00/megaphone.cpp
@@ -3,7 +3,7 @@
 int main(int ac, char *av[])
 {
 	if (ac == 1)
-		std::cout << "shhhhh... I think the students are asleep...";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 		for(int i = 1; av[i]; i++)
 			for (int y = 0; av[i][y]; y++)
