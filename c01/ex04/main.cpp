4b1e293cb93fc4240a1e67c5c00bb7ddf2ca2032
Anton <amyroshn@Antons-MacBook-Pro.local>
2022-05-21 10:41:02 +0200

    zsu

--- c01/ex04/main.cpp
+++ c01/ex04/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <fstream>
 
+void read_file(std::string s, std::ifstream in)
+{
+	std::string tmp;
+	while (getline(in, tmp))
+		s.append(tmp);
+}
+
 int main(int ac, char *av[])
 {
 	if (ac != 4)
@@ -9,14 +16,17 @@ int main(int ac, char *av[])
 		return (1);
 	}
 	std::string infile_name(av[1]);
+	std::string outfile_name(infile_name);
+	outfile_name.append(".replace");
 	std::ofstream out;
 	std::ofstream in;
+	std::string res;
 	in.open(infile_name.data(), std::ios::in);
 	if (!in.is_open()) {
 		std::cout << "Can't open " << infile_name << std::endl;
 		return (2);
 	}
-	out.open(infile_name.append(".replace").data(), std::ios::trunc);
+	out.open(outfile_name.data(), std::ios::trunc);
 	if (!out.is_open()) {
 		in.close();
 		std::cout << "Can't write result in " << infile_name << std::endl;
