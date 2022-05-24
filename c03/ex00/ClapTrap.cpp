2af33902f6929f229a723b15a4e60bff91ba1a99
Anton <amyroshn@Antons-MacBook-Pro.local>
2022-05-24 08:02:09 +0200

    save

---
+++ c03/ex00/ClapTrap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anton on 24/05/2022.
+//
+
+#include "ClapTrap.hpp"
---
+++ c03/ex00/ClapTrap.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Anton on 24/05/2022.
+//
+
+#ifndef EX00_CLAPTRAP_H
+#define EX00_CLAPTRAP_H
+
+
+class ClapTrap {
+
+};
+
+
+#endif //EX00_CLAPTRAP_H
