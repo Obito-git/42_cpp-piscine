d983d6060260a597fd39d9abe179a7b2ab986f82
Anton <amyroshn@Antons-MacBook-Pro.local>
2022-05-24 09:01:22 +0200

    save

--- c03/ex00/ClapTrap.cpp
+++ c03/ex00/ClapTrap.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "ClapTrap.hpp"
+
+void ClapTrap::attack(const std::string &target) {
+
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other) {}
--- c03/ex00/ClapTrap.hpp
+++ c03/ex00/ClapTrap.hpp
@@ -4,11 +4,22 @@
 
 #ifndef EX00_CLAPTRAP_H
 #define EX00_CLAPTRAP_H
-
+#include <string>
 
 class ClapTrap {
+private:
+	std::string _name;
+	unsigned int _hp;
+	unsigned int _energy;
+	unsigned int _attack_dmg;
+public:
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
 
+	ClapTrap();
 };
+
 
 
 #endif //EX00_CLAPTRAP_H
